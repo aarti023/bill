99ba87f08e6901fad9c15d9f05232d9987ec1ff6
Aarti Lather <aarti@thepinchlife.com>
2023-01-02 13:48:22 +0530

    change

--- billing/src/main/java/com/bill/controller/FileController.java
+++ billing/src/main/java/com/bill/controller/FileController.java
@@ -3,7 +3,9 @@ package com.bill.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,8 +14,10 @@ import com.bill.helper.FileUploadHelper;
 
 import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @RestController
+@RequestMapping("/files")
+@Log4j2
+@CrossOrigin(origins = "*")
 public class FileController {
 	
 	@Autowired
