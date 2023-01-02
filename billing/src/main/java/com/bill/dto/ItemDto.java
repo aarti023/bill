b74d1b9869459e706b909d0368e51265ea75d99c
Aarti Lather <aarti@thepinchlife.com>
2023-01-02 13:36:02 +0530

    add items fields

--- billing/src/main/java/com/bill/dto/ItemDto.java
+++ billing/src/main/java/com/bill/dto/ItemDto.java
@@ -10,6 +10,35 @@ import lombok.NoArgsConstructor;
 public class ItemDto {
 	
 	private String invoiceNumber;
-
-	private String item;
+	
+	private String dateOfInvoice;
+		
+	private String source;
+	
+	private String itemCode;
+	
+	private String itemName;
+	
+	private String hSNCode;
+	
+	private String category;
+	
+	private double quantity;
+	
+	private String unit;
+	
+	private double price;
+	
+	private double amount;
+	
+	private String gSTSlab;
+	
+	private double gSTAmount;
+	
+	private double discount;
+	
+	private double redeemed;
+	
+	private double amountPaid;
+	
 }
--- billing/src/main/java/com/bill/model/ItemsEntity.java
+++ billing/src/main/java/com/bill/model/ItemsEntity.java
@@ -30,7 +30,51 @@ public class ItemsEntity {
 	
 	@Column(name = "invoice_number", columnDefinition = "varchar(256)")
 	private String invoiceNumber;
-
-	@Column(name = "item", columnDefinition = "varchar(256)")
-	private String item;
+	
+	@Column(name = "date_of_Invoice", columnDefinition = "varchar(256)")
+	private String dateOfInvoice;
+		
+	@Column(name = "source", columnDefinition = "varchar(256)")
+	private String source;
+	
+	@Column(name = "item_Code", columnDefinition = "varchar(256)")
+	private String itemCode;
+	
+	@Column(name = "item_name", columnDefinition = "varchar(256)")
+	private String itemName;
+	
+	@Column(name = "hsn_sac_code", columnDefinition = "varchar(256)")
+	private String hSNCode;
+	
+	@Column(name = "category", columnDefinition = "varchar(256)")
+	private String category;
+	
+	@Column(name = "unit", columnDefinition = "varchar(256)")
+	private String unit;
+	
+	@Column(name = "quantity", columnDefinition = "varchar(256)")
+	private double quantity;
+	
+	@Column(name = "price", columnDefinition = "varchar(256)")
+	private double price;
+	
+	@Column(name = "amount", columnDefinition = "varchar(256)")
+	private double amount;
+	
+	@Column(name = "gst_slab", columnDefinition = "varchar(256)")
+	private String gSTSlab;
+	
+	@Column(name = "gst_amount", columnDefinition = "varchar(256)")
+	private double gSTAmount;
+	
+	@Column(name = "siscount", columnDefinition = "varchar(256)")
+	private double discount;
+	
+	@Column(name = "amount_paid", columnDefinition = "varchar(256)")
+	private double amountPaid;
+	
+	@Column(name = "redeemed", columnDefinition = "varchar(256)")
+	private double redeemed;
+	
+	
 }
