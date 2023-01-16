9b26fcfc7b118c324d31b347fd7d8714d34fc35d
Aarti Lather <aarti@thepinchlife.com>
2023-01-16 18:37:42 +0530

    changes

--- billing/src/main/java/com/bill/controller/UserController.java
+++ billing/src/main/java/com/bill/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.bill.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bill.dto.InvoiceStatusUpdateDto;
 import com.bill.dto.ResponseDto;
 import com.bill.dto.UserDto;
 import com.bill.dto.UserUpdateDto;
@@ -21,19 +21,19 @@ import com.bill.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
+
 @RestController
 @RequestMapping("/bill")
 @Log4j2
 @CrossOrigin(origins = "*")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	
+
 	@PostMapping("/save")
 	@ApiOperation("save user details")
-	public ResponseDto<UserEntity> saveDetail( @RequestBody UserDto userDto) {
+	public ResponseDto<UserEntity> saveDetail(@RequestBody UserDto userDto) {
 		try {
 			log.info("user {}", userDto);
 			UserEntity response = userService.saveUser(userDto);
@@ -43,7 +43,7 @@ public class UserController {
 			return ResponseDto.failure("Exception occurred while saving the data " + errorMessage);
 		}
 	}
-	
+
 	@GetMapping("/get/data/all")
 	@ApiOperation("get all details")
 	public ResponseDto<List<UserEntity>> getAllDetails() {
@@ -56,10 +56,11 @@ public class UserController {
 			return ResponseDto.failure("Exception occurred while getting the data " + errorMessage);
 		}
 	}
-	
+
 	@PutMapping("/update/{invoiceId}")
 	@ApiOperation("update user details")
-	public ResponseDto<UserUpdateDto> updateDetail(@PathVariable("invoiceId") String invoiceId, @RequestBody UserUpdateDto userUpdateDto) {
+	public ResponseDto<UserUpdateDto> updateDetail(@PathVariable("invoiceId") String invoiceId,
+			@RequestBody UserUpdateDto userUpdateDto) {
 		try {
 			log.info("user {}", invoiceId);
 			UserUpdateDto response = userService.updateUser(invoiceId, userUpdateDto);
@@ -70,19 +71,33 @@ public class UserController {
 		}
 	}
 	
-//	@GetMapping("/get/data/{invoiceNumber}")
-//	@ApiOperation("get detials by id")
-//	public ResponseDto<List<UserEntity>> getDetail(@PathVariable("invoiceNumber") String invoiceNumber) {
-//		try {
-//			log.info("user {}", invoiceNumber);
-//			List<UserEntity> response = userService.findByInvoiceNumber(invoiceNumber);
-//			return ResponseDto.success("user details get successfully", response);
-//		} catch (Exception errorMessage) {
-//			log.error("Exception occurred while getting the data is {}", errorMessage);
-//			return ResponseDto.failure("Exception occurred while getting the data " + errorMessage);
-//		}
-//	}
-	
+	@PutMapping("/update/invoiceStatus/{invoiceId}")
+	@ApiOperation("update reason")
+	public ResponseDto<InvoiceStatusUpdateDto> updateInvoiceStatus(@PathVariable("invoiceId") String invoiceId,
+			@RequestBody InvoiceStatusUpdateDto invoiceStatusUpdateDto) {
+		try {
+			log.info("user {}", invoiceId);
+			InvoiceStatusUpdateDto response = userService.updateInvoiceStatus(invoiceId, invoiceStatusUpdateDto);
+			return ResponseDto.success("user details update successfully", response);
+		} catch (Exception errorMessage) {
+			log.error("Exception occurred while saving the data is {}", errorMessage);
+			return ResponseDto.failure("Exception occurred while updating the data " + errorMessage);
+		}
+	}
+
+	@GetMapping("/get/data/reportingManager/{reportingManagerId}")
+	@ApiOperation("get detials by Reporting Manager Id")
+	public ResponseDto<List<UserEntity>> getDetailByReportingManagerId(@PathVariable("reportingManagerId") String reportingManagerId) {
+		try {
+			log.info("user {}", reportingManagerId);
+			List<UserEntity> response = userService.getDataReportingManagerId(reportingManagerId);
+			return ResponseDto.success("user details get successfully by Reporting ManagerId", response);
+		} catch (Exception errorMessage) {
+			log.error("Exception occurred while getting the data is {}", errorMessage);
+			return ResponseDto.failure("Exception occurred while getting the data " + errorMessage);
+		}
+	}
+
 	@GetMapping("/get/data/{employeeCode}")
 	@ApiOperation("get detials by employee Code")
 	public ResponseDto<List<UserEntity>> getDetail(@PathVariable("employeeCode") String employeeCode) {
@@ -95,8 +110,7 @@ public class UserController {
 			return ResponseDto.failure("Exception occurred while getting the data " + errorMessage);
 		}
 	}
-	
-	
+
 //	@GetMapping("/get/name/email/{employeeCode}")
 //	@ApiOperation("get name and email by employeeCode")
 //	public ResponseDto<EmployeeCodeDto> getDetailByEmployeeCode(@PathVariable("employeeCode") String employeeCode) {
@@ -110,7 +124,6 @@ public class UserController {
 //		}
 //	}
 
-	
 //	@GetMapping("/gets/")
 //	@ApiOperation("get all details of merge data")
 //	public ResponseDto<List<MergeAllTableDto>> getAll() {
@@ -123,7 +136,5 @@ public class UserController {
 //			return ResponseDto.failure("Exception occurred while getting the data " + errorMessage);
 //		}
 //	}
-	
-	
-	
+
 }
