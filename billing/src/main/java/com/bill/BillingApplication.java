a1ff6db59581e22fb6159a58cea9669ac781af91
Aarti Lather <aarti@thepinchlife.com>
2023-01-30 15:12:05 +0530

    changes

--- billing/src/main/java/com/bill/BillingApplication.java
+++ billing/src/main/java/com/bill/BillingApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@Component("")
 public class BillingApplication {
 
 	public static void main(String[] args) {
